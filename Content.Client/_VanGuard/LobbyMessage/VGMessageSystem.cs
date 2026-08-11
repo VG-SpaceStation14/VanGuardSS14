4ded40a010c42e287c659cbd4d37f86f1ddddbdd
Albuka1 <b.v.v03092013@gmail.com>
2026-08-11 18:24:02 +0300

    Fix lobby message request double net-message registration (use network event instead)

--- Content.Client/_VanGuard/LobbyMessage/VGMessageSystem.cs
+++ Content.Client/_VanGuard/LobbyMessage/VGMessageSystem.cs
@@ -1,13 +1,11 @@
 using Content.Shared._VanGuard.LobbyMessage;
 using Robust.Client.State;
-using Robust.Shared.Network;
 
 namespace Content.Client._VanGuard.LobbyMessage;
 
 public sealed partial class VGMessageSystem : EntitySystem
 {
     [Dependency] private IStateManager _state = default!;
-    [Dependency] private IClientNetManager _net = default!;
 
     private VGMessagePanel? _panel;
 
@@ -15,7 +13,6 @@ public sealed partial class VGMessageSystem : EntitySystem
     {
         base.Initialize();
 
-        _net.RegisterNetMessage<MsgVGMessageRequest>();
         SubscribeNetworkEvent<VGMessageEvent>(OnMessage);
         _state.OnStateChanged += OnStateChanged;
     }
@@ -36,8 +33,7 @@ public sealed partial class VGMessageSystem : EntitySystem
             }
 
             // Запрашиваем текущее сообщение лобби у сервера
-            var request = _net.CreateNetMessage<MsgVGMessageRequest>();
-            _net.ClientSendMessage(request);
+            RaiseNetworkEvent(new MsgVGMessageRequest());
         }
         else
         {
